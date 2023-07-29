aebcf53423500504c5e5ea257be3f86f06fd5e6b
servet-isikli <servetisikli@yahoo.com>
2023-07-29 22:30:16 +0200

    grid added

--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -6,7 +6,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { Container } from "@mui/material";
+import { Container, Grid } from "@mui/material"; // Import Grid component from Material-UI
 
 const MediaCard = () => {
   const [estates, setEstates] = useState([]);
@@ -32,32 +32,40 @@ const MediaCard = () => {
   return (
     <Container>
       <h1>Estates</h1>
-      {estates.map((estate) => (
-        <Card key={estate.id} sx={{ maxWidth: 345 }}>
-          <CardMedia
-            sx={{ height: 140 }}
-            component="img"
-            image={estate.attributes.Image}
-            title={estate.attributes.Name}
-            onError={(e) => {
-              e.target.onerror = null;
-              e.target.src = "../assets/images/cards/img2.png";
-            }}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {estate.attributes.Name}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {estate.attributes.Description}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Share</Button>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </Card>
-      ))}
+      <Grid container spacing={2}>
+        {" "}
+        {/* Use Grid container to create a row */}
+        {estates.map((estate) => (
+          <Grid item key={estate.id} xs={12} sm={6} md={4} lg={3}>
+            {" "}
+            {/* Each Card will take 6 columns on small screens, 4 columns on medium screens, and 3 columns on large screens */}
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                sx={{ height: 140 }}
+                component="img"
+                image={estate.attributes.Image}
+                title={estate.attributes.Name}
+                onError={(e) => {
+                  e.target.onerror = null;
+                  e.target.src = "../assets/images/cards/img2.png";
+                }}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {estate.attributes.Name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {estate.attributes.Description}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </Container>
   );
 };
