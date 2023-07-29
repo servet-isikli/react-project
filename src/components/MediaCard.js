f56ce4f0c3e90589993e771795741fea467c2159
servet-isikli <servetisikli@yahoo.com>
2023-07-29 20:21:09 +0200

    edit

--- src/components/MediaCard.js
+++ src/components/MediaCard.js
@@ -1,32 +1,68 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { Container } from "@mui/material";
 
-export default function MediaCard() {
+const MediaCard = () => {
+  const [estates, setEstates] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  console.log(estates);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:1337/api/estates")
+      .then((response) => {
+        console.log(response);
+        setEstates(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching estates:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <Container >
+      <h1>Estates</h1>
+      {estates.map((estate) => (
+        <Card key={estate.id} sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            component="img"
+            image={estate.attributes.Image}
+            title={estate.attributes.Name}
+            onError={(e) => {
+              e.target.onerror = null;
+              e.target.src = "../assets/images/cards/img2.png";
+            }}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {estate.attributes.Name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {estate.attributes.Description}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      ))}
+    </Container>
   );
-}
+};
+
+export default MediaCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,13 @@ import React from "react";
 import PrimarySearchAppBar from "../components/PrimarySearchAppBar";
 import MediaCard from "../components/MediaCard";
 import BasicGrid from "../components/BasicGrid";
-import EstateList from "../components/EstateList";
 
 const Home = () => {
   return (
     <div>
       <PrimarySearchAppBar />
-      <MediaCard />
       <BasicGrid />
-      <EstateList/>
+      <MediaCard />
     </div>
   );
 };
